cebf539590a2ec9a305c3331b4b1d3a8daa964c2
samsulai <samuelsulai2015@gmail.com>
2022-05-27 09:21:10 +0100

    made changes to the hero page

--- components/hero.js
+++ components/hero.js
@@ -41,7 +41,7 @@ OpenSea is the World&apos;s first and largest NFT marketplace.
 className="h-[2.25rem] rounded-full" src="https://lh3.googleusercontent.com/qQj55gGIWmT1EnMmGQBNUpIaj0qTyg4YZSQ2ymJVvwr_mXXjuFiHJG9d3MRgj5DVgyLa69u8Tq9ijSm_stsph8YmIJlJQ1e7n6xj=s64" alt=""
 />
 <div className={style.author}>
-<div className={style.name}>Sammycodes</div>
+<div className={style.name}>Bospectibles</div>
 <a className="text-[#1868b7]" href="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/2324922113504035910649522729980423429926362207300810036887725141691069366277">Sammy-codes</a>
 
 </div>
